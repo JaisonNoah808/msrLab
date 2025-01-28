16870b48ffb895d2ab4e34fd73d3075ed6942869
JaisonNoah <jaisonnoah123@gmail.com>
2025-01-28 12:56:27 -0800

    Fixed scatter plot to be like example and more readable

--- repo_mining/JaisonSarte_scatterplot.py
+++ repo_mining/JaisonSarte_scatterplot.py
@@ -11,8 +11,10 @@ import random
 extractedData = pd.read_csv("data/data_rootbeer.csv")
 # Converting Date into a datetime object
 extractedData["Date"] = pd.to_datetime(extractedData["Date"])
-# Getting the weeks from the datetime object we converted
-extractedData["Week"] = extractedData["Date"].dt.isocalendar().week # Week of the year
+
+# Calculate the number of weeks since the start of development
+start_date = extractedData["Date"].min()
+extractedData["Week"] = ((extractedData["Date"] - start_date).dt.days // 7)
 
 # Getting singular files, no dupes of the same file
 allFiles = np.unique(extractedData["File"])
@@ -27,7 +29,7 @@ extractedData["File"] = extractedData["File"].map(fileIndex)
 # Author Colors/Shades 
 # Getting all the authors, no dupes
 authors = np.unique(extractedData["Author"])
-# Giving each author a random 
+# Giving each author a random color
 colorMap = {}
 
 # For each author, generate a random color
@@ -47,23 +49,23 @@ plt.figure(figsize=(14, 14))
 for author in authors:
     # Creating mask for data of only the current author
     mask = (theAuthors == author)
-    # Getting that authors # touches each week
+    # Getting that authors touches each week
     theWeeksAuthor = theWeeks[mask]
     # Same for files
     theFilesAuthor = theFiles[mask]
     # Getting the color of the author
     theColor = colorMap[author]
     # Plotting the data
-    plt.scatter(theWeeksAuthor, theFilesAuthor, c=theColor, alpha=0.5, edgecolor="black", label=author)
+    plt.scatter(theFilesAuthor, theWeeksAuthor, c=theColor, alpha=0.5, edgecolor="black", label=author)
 
-    # Adding legend to display author names
-    plt.legend(title="Authors", bbox_to_anchor=(1.05, 1), loc='upper left')
-
+# Adding legend to display author names
+plt.legend(title="Authors", bbox_to_anchor=(1.05, 1), loc='upper left')
 
 # Labeling the plot
 plt.title("Author File Touches by Week")
-plt.xlabel("Week")
-plt.ylabel("File Value")
+plt.xlabel("File Value")
+plt.ylabel("Weeks")
+y_ticks = np.arange(0, theWeeks.max() + 50, 50)  # Set y-axis ticks to go by 50's
+plt.yticks(y_ticks) # Makes it more readable
 plt.grid(alpha=0.3)
-plt.tight_layout()
 plt.show()
